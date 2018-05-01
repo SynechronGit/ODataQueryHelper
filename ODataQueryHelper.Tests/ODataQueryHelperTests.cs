92ed0dda085ea79ca9fcca3e536726c6c4e43c3a
Sanket Mistry <Sanket.Mistry@synechron.com>
2018-05-01 01:02:31 -0400

    Fix build errors

--- ODataQueryHelper.Tests/ODataQueryHelperTests.cs
+++ ODataQueryHelper.Tests/ODataQueryHelperTests.cs
@@ -1,18 +1,12 @@
 using MongoDB.Bson;
-using MongoDB.Bson.Serialization;
-using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson.Serialization.Conventions;
 using MongoDB.Driver;
 using MongoDB.Driver.Core.Events;
 using ODataQueryHelper.Core;
-using ODataQueryHelper.MongoDB;
-using StringToExpression.GrammerDefinitions;
-using StringToExpression.LanguageDefinitions;
+using ODataQueryRunner.MongoDB;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Xunit;
 
