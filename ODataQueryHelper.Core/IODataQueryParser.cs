d129e9d02896702b78aca24bf1ddaf1d7c4a7e3d
Sanket Mistry <Sanket.Mistry@synechron.com>
2018-05-08 11:41:40 -0400

    - Expose Interface for ODataQueryParser

---
+++ ODataQueryHelper.Core/IODataQueryParser.cs
@@ -0,0 +1,18 @@
+﻿using ODataQueryHelper.Core.Model;
+
+namespace ODataQueryHelper.Core
+{
+    /// <summary>
+    /// OData query parser for Document
+    /// </summary>
+    /// <typeparam name="T">Document of type <typeparamref name="T"/></typeparam>
+    public interface IODataQueryParser<T> where T : class
+    {
+        /// <summary>
+        /// Try and Parse OData Query expression
+        /// </summary>
+        /// <param name="oDataExpression">OData standard query expression</param>
+        /// <returns>Parser Document Query of type <typeparamref name="T"/></returns>
+        DocumentQuery<T> TryParse(string oDataExpression);
+    }
+}
--- ODataQueryHelper.Core/ODataQueryParser.cs
+++ ODataQueryHelper.Core/ODataQueryParser.cs
@@ -4,8 +4,17 @@ using System.Web;
 
 namespace ODataQueryHelper.Core
 {
-    public class ODataQueryParser<T> where T: class
+    /// <summary>
+    /// OData query parser for Document
+    /// </summary>
+    /// <typeparam name="T">Document of type <typeparamref name="T"/></typeparam>
+    public class ODataQueryParser<T> : IODataQueryParser<T> where T: class
     {
+        /// <summary>
+        /// Try and Parse OData Query expression
+        /// </summary>
+        /// <param name="oDataExpression">OData standard query expression</param>
+        /// <returns>Parser Document Query of type <typeparamref name="T"/></returns>
         public DocumentQuery<T> TryParse(string oDataExpression)
         {
             var model = new DocumentQuery<T>();
